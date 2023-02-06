c9e212a3575f5636879a71ae09dfcf09906d40bc
felipejoq <felipejofreq@gmail.com>
2023-02-06 13:02:58 -0300

    Add modal time out and blocked game zone when time is over

--- src/js/actions.js
+++ src/js/actions.js
@@ -1,20 +1,36 @@
 import {score, time} from "./elements.html";
+import {MODAL} from "./popups";
 let points = 0;
+let gameZone = document.getElementById("game-zone");
+let timeOut = document.getElementById("time-out");
 
 export const timer = () => {
     time.innerHTML = "⌛️ 0";
+    timeOut.style.visibility = "hidden";
     let count = 0;
+    let limit = 60;
     let intervalID = setInterval(() => {
         time.innerHTML = "";
         time.innerHTML = `⌛️ ${count += 1}`;
-        if (count >= 60) {
-            alert('¡Time out!')
-            return clearInterval(intervalID)
+        if (count >= limit) {
+            console.log('Valor de count: ', count)
+            gameZone.classList.add("disabled");
+            timeOut.style.visibility = "visible";
+            MODAL.generic('Time Out!', 'Your time is over. :(', "error", 'Try again?')
+                .then(result => {
+                    if(result.isConfirmed) {
+                        console.log("User confirm try again");
+                    }
+                });
+            return clearInterval(intervalID);
         }
     }, 1000);
 }
 export const addScore = (point) => {
     points += point;
+    if(points === 8) {
+        MODAL.generic('You Win!', 'Wow, your mind is very powerful!', "success", 'Great!', 'Accept')
+    }
     score.innerHTML = `🎯 ${ points }`
 }
 export const sounds = () => {
